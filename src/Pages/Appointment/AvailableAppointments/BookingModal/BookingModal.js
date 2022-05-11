a8d385d0e252baa50ef8b8168ec70cb9d8a54ff1
Mohammad Tanim <mohammad.tanim24h@gmail.com>
2022-05-11 19:24:34 +0600

    slot added in modal form

--- src/Pages/Appointment/AvailableAppointments/BookingModal/BookingModal.js
+++ src/Pages/Appointment/AvailableAppointments/BookingModal/BookingModal.js
@@ -1,7 +1,19 @@
+import { format } from "date-fns";
 import React from "react";
 
-const BookingModal = ({treatment}) => {
+const BookingModal = ({treatment, date, setTreatment}) => {
     const {slots, name} = treatment;
+    
+    const handleBooking = e => {
+        e.preventDefault();
+        const slot = e.target.slot.value;
+        const name = e.target.name.value;
+        const phone = e.target.phone.value;
+        const email = e.target.email.value;
+        
+
+        setTreatment(null);
+    }
     return (
         <div>
             <input type="checkbox" id="booking-modal" className="modal-toggle" />
@@ -11,12 +23,16 @@ const BookingModal = ({treatment}) => {
                     <h3 className="font-bold text-lg text-secondary mb-6">
                         {name}
                     </h3>
-                    <form className="grid grid-cols-1 gap-3 justify-items-center">
-                        <input type="text" placeholder="Type here" className="input input-bordered w-full max-w-lg" />
-                        <input type="text" placeholder="Type here" className="input input-bordered w-full max-w-lg" />
-                        <input type="text" placeholder="Full Name" className="input input-bordered w-full max-w-lg" />
-                        <input type="number" placeholder="Phone Number" className="input input-bordered w-full max-w-lg" />
-                        <input type="email" placeholder="Email" className="input input-bordered w-full max-w-lg" />
+                    <form onSubmit={handleBooking} className="grid grid-cols-1 gap-3 justify-items-center">
+                        <input type="text" value={date && format(date, "PP")} disabled className="input input-bordered w-full max-w-lg" />
+                        {/* Time Slot Dropdown start */}
+                        <select name="slot" className="select select-bordered w-full max-w-lg">
+                            {slots.map(slot => <option value={slot}>{slot}</option>)}
+                        </select>
+                        {/* Time Slot Dropdown end */}
+                        <input type="text" name="name" placeholder="Full Name" className="input input-bordered w-full max-w-lg" required />
+                        <input type="number" name="phone" placeholder="Phone Number" className="input input-bordered w-full max-w-lg" required />
+                        <input type="email" name="email" placeholder="Email" className="input input-bordered w-full max-w-lg" required />
                         <input type="submit" value="Book" className="btn btn-secondary w-full max-w-lg  px-8 py-2 text-white" />
                     </form>
                 </div>
