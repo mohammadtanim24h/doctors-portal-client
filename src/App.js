53419273acbe63c648a1721b2db3a6e154eaae29
Mohammad Tanim <mohammad.tanim24h@gmail.com>
2022-05-13 01:37:07 +0600

    require auth component added

--- src/App.js
+++ src/App.js
@@ -5,6 +5,7 @@ import Appointment from "./Pages/Appointment/Appointment";
 import Home from "./Pages/HomePage/Home/Home";
 import Login from "./Pages/Login/Login";
 import Navbar from "./Pages/Shared/Navbar/Navbar";
+import RequireAuth from "./Pages/Shared/RequireAuth/RequireAuth";
 import SignUp from "./Pages/SignUp/SignUp";
 
 function App() {
@@ -13,7 +14,14 @@ function App() {
             <Navbar></Navbar>
             <Routes>
                 <Route path="/" element={<Home></Home>}></Route>
-                <Route path="/appointment" element={<Appointment></Appointment>}></Route>
+                <Route
+                    path="/appointment"
+                    element={
+                        <RequireAuth>
+                            <Appointment></Appointment>
+                        </RequireAuth>
+                    }
+                ></Route>
                 <Route path="/about" element={<About></About>}></Route>
                 <Route path="/login" element={<Login></Login>}></Route>
                 <Route path="/signup" element={<SignUp></SignUp>}></Route>
