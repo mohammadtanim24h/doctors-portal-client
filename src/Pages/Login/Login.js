01b84cf62474183ad33dfdd283883fbbf971c407
Mohammad Tanim <mohammad.tanim24h@gmail.com>
2022-05-12 13:31:28 +0600

    added .env for firebase config and google sign in added

--- src/Pages/Login/Login.js
+++ src/Pages/Login/Login.js
@@ -1,9 +1,24 @@
-import React from 'react';
+import React from "react";
+import auth from "../../Firebase/firebase.init";
+import { useSignInWithGoogle } from 'react-firebase-hooks/auth';
 
 const Login = () => {
+    const [signInWithGoogle, user, loading, error] = useSignInWithGoogle(auth);
+    if(user) {
+        console.log(user);
+    }
+    if(error) {
+        console.error(error);
+    }
     return (
-        <div>
-            <h2 className='text-5xl text-center'>Login</h2>
+        <div className="flex justify-center items-center mt-24">
+            <div className="card w-96 shadow-xl">
+                <div className="card-body text-center">
+                    <h2 className="card-title justify-center mb-16">Login</h2>
+                    <div className="divider">OR</div>
+                    <button className="btn btn-outline" onClick={() => signInWithGoogle()}>Continue With Google</button>
+                </div>
+            </div>
         </div>
     );
 };
