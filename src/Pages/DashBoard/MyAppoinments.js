7fc79d025004ad282a78406a88e0b5360c163dba
Mohammad Tanim <mohammad.tanim24h@gmail.com>
2022-05-15 04:13:37 +0600

    my appointments table added

--- src/Pages/DashBoard/MyAppoinments.js
+++ src/Pages/DashBoard/MyAppoinments.js
@@ -1,6 +1,6 @@
-import React, { useEffect, useState } from 'react';
-import { useAuthState } from 'react-firebase-hooks/auth';
-import auth from '../../Firebase/firebase.init';
+import React, { useEffect, useState } from "react";
+import { useAuthState } from "react-firebase-hooks/auth";
+import auth from "../../Firebase/firebase.init";
 
 const MyAppoinments = () => {
     const [appointments, setAppoinments] = useState([]);
@@ -8,17 +8,38 @@ const MyAppoinments = () => {
     const email = user.email;
     useEffect(() => {
         fetch(`http://localhost:5000/booking?email=${email}`)
-        .then(res => res.json())
-        .then(data => {
-            setAppoinments(data);
-        })
-    }, [email])
-
-    console.log(appointments);
+            .then((res) => res.json())
+            .then((data) => {
+                setAppoinments(data);
+            });
+    }, [email]);
 
     return (
         <div>
-            <h2 className='text-purple-500 text-2xl'>My Appointments</h2>
+            <h2 className="text-purple-500 text-2xl">My Appointments</h2>
+            <div class="overflow-x-auto mt-6">
+                <table class="table w-full">
+                    {/* <!-- head --> */}
+                    <thead>
+                        <tr>
+                            <th>Name</th>
+                            <th>Date</th>
+                            <th>Time</th>
+                            <th>Treatment</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        {appointments.map((a) => (
+                            <tr key={a._id}>
+                                <td>{a.patientName}</td>
+                                <td>{a.date}</td>
+                                <td>{a.slot}</td>
+                                <td>{a.treatment}</td>
+                            </tr>
+                        ))}
+                    </tbody>
+                </table>
+            </div>
         </div>
     );
 };
