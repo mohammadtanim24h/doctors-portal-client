aa38c9996822fa2be5473b9abcbb56897758ffae
Mohammad Tanim <mohammad.tanim24h@gmail.com>
2022-05-14 19:55:13 +0600

    react query initial setup done

--- src/index.js
+++ src/index.js
@@ -5,13 +5,24 @@ import App from "./App";
 import reportWebVitals from "./reportWebVitals";
 import { BrowserRouter } from "react-router-dom";
 
+// React Query
+import {
+    QueryClient,
+    QueryClientProvider,
+} from "react-query";
+
+// Creating client
+const queryClient = new QueryClient();
+
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
-    <BrowserRouter>
-        <React.StrictMode>
-            <App />
-        </React.StrictMode>
-    </BrowserRouter>
+    <QueryClientProvider client={queryClient}>
+        <BrowserRouter>
+            <React.StrictMode>
+                <App />
+            </React.StrictMode>
+        </BrowserRouter>
+    </QueryClientProvider>
 );
 
 // If you want to start measuring performance in your app, pass a function
