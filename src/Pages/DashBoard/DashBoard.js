2f7e1e9b80554d86ea7cba93b22cdfc83333ddda
Mohammad Tanim <mohammad.tanim24h@gmail.com>
2022-05-15 04:01:59 +0600

    dashboard svg icon added in the nav

--- src/Pages/DashBoard/DashBoard.js
+++ src/Pages/DashBoard/DashBoard.js
@@ -4,19 +4,13 @@ import { Link, Outlet } from "react-router-dom";
 const DashBoard = () => {
     return (
         <div class="drawer drawer-mobile">
-            <input id="my-drawer-2" type="checkbox" class="drawer-toggle" />
+            <input id="dashboard-sidebar" type="checkbox" class="drawer-toggle" />
             <div class="drawer-content flex flex-col">
-                <h2 className="text-3xl text-purple-500">Dashboard</h2>
+                <h2 className="text-3xl text-purple-500 font-bold">Dashboard</h2>
                 <Outlet></Outlet>
-                <label
-                    for="my-drawer-2"
-                    class="btn btn-primary drawer-button lg:hidden"
-                >
-                    Open drawer
-                </label>
             </div>
             <div class="drawer-side">
-                <label for="my-drawer-2" class="drawer-overlay"></label>
+                <label for="dashboard-sidebar" class="drawer-overlay"></label>
                 <ul class="menu p-4 overflow-y-auto w-80 bg-base-100 text-base-content">
                     {/* <!-- Sidebar content here --> */}
                     <li>
--- src/Pages/DashBoard/MyAppoinments.js
+++ src/Pages/DashBoard/MyAppoinments.js
@@ -1,6 +1,21 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+import { useAuthState } from 'react-firebase-hooks/auth';
+import auth from '../../Firebase/firebase.init';
 
 const MyAppoinments = () => {
+    const [appointments, setAppoinments] = useState([]);
+    const [user] = useAuthState(auth);
+    const email = user.email;
+    useEffect(() => {
+        fetch(`http://localhost:5000/booking?email=${email}`)
+        .then(res => res.json())
+        .then(data => {
+            setAppoinments(data);
+        })
+    }, [email])
+
+    console.log(appointments);
+
     return (
         <div>
             <h2 className='text-purple-500 text-2xl'>My Appointments</h2>
