0a7337d36fc41960cd785ac57f47b5b70fe7f544
Mohammad Tanim <mohammad.tanim24h@gmail.com>
2022-05-18 13:24:17 +0600

    added doctor deletion feature

--- src/Pages/DashBoard/DoctorRow.js
+++ src/Pages/DashBoard/DoctorRow.js
@@ -1,24 +1,43 @@
 import React from "react";
+import { toast } from "react-toastify";
 
 const DoctorRow = ({ doctor, index, refetch }) => {
-    const { name, img, specialty } = doctor;
+    const { name, img, specialty, email } = doctor;
+
+    const handleDelete = (email) => {
+        fetch(`http://localhost:5000/doctor/${email}`, {
+            method: "DELETE",
+            headers: {
+                authorization: `Bearer ${localStorage.getItem("accessToken")}`,
+            },
+        })
+            .then((res) => res.json())
+            .then((data) => {
+                if (data.deletedCount === 1) {
+                    toast.success(`Doctor ${name} was deleted successfully`);
+                    refetch();
+                }
+                console.log(data);
+            });
+    };
+
     return (
         <tr>
             <th>{index + 1}</th>
             <td>
                 <div class="avatar">
                     <div class="w-16 rounded">
-                        <img
-                            src={img}
-                            alt="doctor"
-                        />
+                        <img src={img} alt="doctor" />
                     </div>
                 </div>
             </td>
             <td>{name}</td>
             <td>{specialty}</td>
             <td>
-                <button class="btn btn-xs bg-red-500 outline-0 border-0 text-white hover:bg-red-500 px-4">
+                <button
+                    onClick={() => handleDelete(email)}
+                    class="btn btn-xs bg-red-500 outline-0 border-0 text-white hover:bg-red-500 px-4"
+                >
                     Delete
                 </button>
             </td>
